c92a9ba4cfd1332c0e4bd5923899af879e7b67e2
scyse <scyse@scyses-MBP.lan>
2022-02-07 08:41:36 +0100

    Changes fir ISB Provider Support Signing

--- modules/cesecore-common/src/org/cesecore/keys/token/ISBProvider.java
+++ modules/cesecore-common/src/org/cesecore/keys/token/ISBProvider.java
@@ -163,7 +163,7 @@ public class ISBProvider extends Provider {
                 map.put("keyId", this.privateKey.getKeyURL());
                 map.put("ringId", this.privateKey.getCryptoToken().getClientID());
                 map.put("algorithm", azureSignAlg);
-                map.put("data", Base64.encodeBase64(signInput));
+                map.put("data", new String(Base64.encodeBase64(signInput)));
                 final JSONObject jsonObject = new JSONObject(map);
                 final StringWriter out = new StringWriter();
                 jsonObject.writeJSONString(out);
